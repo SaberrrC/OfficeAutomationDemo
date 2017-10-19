b09cf1565fbc0fcde617e2a807540aaf9387d2c6
v_wangfengxu <Tonnywfx@gmail.com>
2017-10-19 15:44:30 +0800

    update code

--- app/src/main/java/com/shanlinjinrong/oa/ui/activity/home/schedule/meetingdetails/MeetingDetailsActivity.java
+++ app/src/main/java/com/shanlinjinrong/oa/ui/activity/home/schedule/meetingdetails/MeetingDetailsActivity.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.TextView;
 
+import com.iflytek.cloud.thirdparty.V;
+import com.jakewharton.rxbinding2.view.RxView;
 import com.shanlinjinrong.oa.R;
 import com.shanlinjinrong.oa.ui.activity.home.schedule.meetingdetails.adapter.MeetingDetailsAdapter;
 import com.shanlinjinrong.oa.ui.activity.home.schedule.meetingdetails.bean.MeetingRoomsBean;
@@ -17,9 +21,13 @@ import com.shanlinjinrong.views.common.CommonTopView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
 
 /**
  * 选择会议室
@@ -32,6 +40,8 @@ public class MeetingDetailsActivity extends HttpBaseActivity<MeetingDetailsActiv
     RecyclerView mMeetingDetailsList;
     @Bind(R.id.refresh_layout)
     SwipeRefreshLayout mRefreshLayout;
+    @Bind(R.id.tv_network_error)
+    TextView mTvNetworkError;
     private MeetingDetailsAdapter mMeetingRoomAdapter;
     private List<MeetingRoomsBean.DataBean> data = new ArrayList<>();
 
@@ -51,6 +61,7 @@ public class MeetingDetailsActivity extends HttpBaseActivity<MeetingDetailsActiv
 
     private void initView() {
         mTopView.getRightView().setOnClickListener(this);
+        mRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_dark);
         mMeetingDetailsList.setLayoutManager(new LinearLayoutManager(this));
         mMeetingRoomAdapter = new MeetingDetailsAdapter(this, data);
         mMeetingDetailsList.setAdapter(mMeetingRoomAdapter);
@@ -64,18 +75,41 @@ public class MeetingDetailsActivity extends HttpBaseActivity<MeetingDetailsActiv
 
     @Override
     public void onClick(View view) {
-        Intent intent = new Intent(this, MeetingReservationRecordActivity.class);
-        startActivity(intent);
+        switch (view.getId()) {
+            case com.shanlinjinrong.uilibrary.R.id.topview_right_view:
+                Intent intent = new Intent(this, MeetingReservationRecordActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void getMeetingRoomsEmpty() {
+        mTvNetworkError.setVisibility(View.VISIBLE);
+        mTvNetworkError.setText(R.string.string_meeting_rooms_not_available);
     }
 
     @Override
     public void getMeetingRoomsSuccess(List<MeetingRoomsBean.DataBean> data) {
+        mTvNetworkError.setVisibility(View.GONE);
         mMeetingRoomAdapter.setNewData(data);
         mRefreshLayout.setRefreshing(false);
     }
 
     @Override
-    public void getMeetingRoomsFailed(String data) {
+    public void getMeetingRoomsFailed(int errorCode, String data) {
+        switch (errorCode) {
+            case -1:
+                mTvNetworkError.setText(R.string.string_not_network);
+                mTvNetworkError.setVisibility(View.VISIBLE);
+                mTvNetworkError.setOnClickListener(this);
+                break;
+            default:
+                mTvNetworkError.setVisibility(View.GONE);
+                break;
+        }
         mRefreshLayout.setRefreshing(false);
     }
 
--- app/src/main/java/com/shanlinjinrong/oa/ui/activity/home/schedule/meetingdetails/concract/MeetingDetailsActivityContract.java
+++ app/src/main/java/com/shanlinjinrong/oa/ui/activity/home/schedule/meetingdetails/concract/MeetingDetailsActivityContract.java
@@ -7,17 +7,18 @@ import com.shanlinjinrong.oa.ui.base.BaseView;
 import java.util.List;
 
 /**
- * 获取会议详情
- *
+ * Created by tonny on 2017/10/11.
  */
 
 public interface MeetingDetailsActivityContract {
 
     interface View extends BaseView {
 
+        void getMeetingRoomsEmpty();
+
         void getMeetingRoomsSuccess(List<MeetingRoomsBean.DataBean> data);
 
-        void getMeetingRoomsFailed(String data);
+        void getMeetingRoomsFailed(int errorCode,String data);
     }
 
     interface Presenter extends BasePresenter<MeetingDetailsActivityContract.View> {
