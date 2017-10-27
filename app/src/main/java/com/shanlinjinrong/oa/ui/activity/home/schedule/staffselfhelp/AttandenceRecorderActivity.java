5856b8c769ee16bc8f9b2b16fd81d88a2e88cc1e
v_wangfengxu <Tonnywfx@gmail.com>
2017-10-27 11:27:31 +0800

    考勤Ui

---
+++ app/src/main/java/com/shanlinjinrong/oa/ui/activity/home/schedule/staffselfhelp/AttandenceRecorderActivity.java
@@ -0,0 +1,44 @@
+package com.shanlinjinrong.oa.ui.activity.home.schedule.staffselfhelp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.shanlinjinrong.oa.R;
+import com.shanlinjinrong.oa.ui.base.BaseActivity;
+import com.shanlinjinrong.views.common.CommonTopView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Administrator on 2017/10/27 0027.
+ */
+
+public class AttandenceRecorderActivity extends BaseActivity{
+    @Bind(R.id.top_view)
+    CommonTopView mTopView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attandance_recorder_list);
+        ButterKnife.bind(this);
+        initData();
+        initView();
+    }
+
+    private void initData() {
+
+
+    }
+
+    private void initView() {
+        mTopView.setRightDrawable(getResources().getDrawable(R.drawable.icon_login_user));
+        View leftView = mTopView.getLeftView();
+        leftView.setOnClickListener(view -> {
+            finish();
+        });
+    }
+
+}
