2d2d18312d2e5271154644e6a1f1537194c81faf
v_chumingyuan <saberrrc@163.com>
2017-11-14 18:01:15 +0800

    [mingyuan.chu]修改崩溃bug

--- app/src/main/java/com/shanlinjinrong/oa/ui/activity/upcomingtasks/UpcomingTasksInfoActivity.java
+++ app/src/main/java/com/shanlinjinrong/oa/ui/activity/upcomingtasks/UpcomingTasksInfoActivity.java
@@ -563,30 +563,38 @@ public class UpcomingTasksInfoActivity extends HttpBaseActivity<UpcomingTasksInf
                         billType = mSearchBean.getPkBillType();
                     }
                     if (TextUtils.equals(billType, "6402")) {//签卡申请
-                        if (mCardResultBean.getData() != null) {
-                            if (mCardResultBean.getData().getApplyWorkFlows() != null) {
-                                mDatas.addAll(mCardResultBean.getData().getApplyWorkFlows());
+                        if (mCardResultBean != null) {
+                            if (mCardResultBean.getData() != null) {
+                                if (mCardResultBean.getData().getApplyWorkFlows() != null) {
+                                    mDatas.addAll(mCardResultBean.getData().getApplyWorkFlows());
+                                }
                             }
                         }
                     }
                     if (TextUtils.equals(billType, "6403")) {//出差申请
-                        if (mTraverResultBean.getData() != null) {
-                            if (mTraverResultBean.getData().getNchrapplyWorkFlow() != null) {
-                                mDatas.addAll(mTraverResultBean.getData().getNchrapplyWorkFlow());
+                        if (mTraverResultBean != null) {
+                            if (mTraverResultBean.getData() != null) {
+                                if (mTraverResultBean.getData().getNchrapplyWorkFlow() != null) {
+                                    mDatas.addAll(mTraverResultBean.getData().getNchrapplyWorkFlow());
+                                }
                             }
                         }
                     }
                     if (TextUtils.equals(billType, "6404")) {//休假申请
-                        if (mRestResultBean.getData() != null) {
-                            if (mRestResultBean.getData().getNchrapplyWorkFlow() != null) {
-                                mDatas.addAll(mRestResultBean.getData().getNchrapplyWorkFlow());
+                        if (mRestResultBean != null) {
+                            if (mRestResultBean.getData() != null) {
+                                if (mRestResultBean.getData().getNchrapplyWorkFlow() != null) {
+                                    mDatas.addAll(mRestResultBean.getData().getNchrapplyWorkFlow());
+                                }
                             }
                         }
                     }
                     if (TextUtils.equals(billType, "6405")) {//加班申请
-                        if (mOverTimeResultBean.getData() != null) {
-                            if (mOverTimeResultBean.getData().getNchrapplyWorkFlow() != null) {
-                                mDatas.addAll(mOverTimeResultBean.getData().getNchrapplyWorkFlow());
+                        if (mOverTimeResultBean != null) {
+                            if (mOverTimeResultBean.getData() != null) {
+                                if (mOverTimeResultBean.getData().getNchrapplyWorkFlow() != null) {
+                                    mDatas.addAll(mOverTimeResultBean.getData().getNchrapplyWorkFlow());
+                                }
                             }
                         }
                     }
