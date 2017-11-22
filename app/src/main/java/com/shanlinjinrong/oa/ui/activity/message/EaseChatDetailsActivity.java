eb5381a58bdf9e477d3c8fd2d717b3cb2a40c063
v_wangfengxu <Tonnywfx@gmail.com>
2017-11-22 09:50:11 +0800

    update code

---
+++ app/src/main/java/com/shanlinjinrong/oa/ui/activity/message/EaseChatDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.shanlinjinrong.oa.ui.activity.message;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shanlinjinrong.oa.R;
+
+public class EaseChatDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ease_chat_details);
+    }
+}
