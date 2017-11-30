70abd39810357e25eefc931c379b4e52fcd1b1d5
huanghaibin <328938999@qq.com>
2017-11-30 09:47:03 +0800

    环境改变

--- retrofit/src/main/java/com/example/retrofit/net/ApiConstant.java
+++ retrofit/src/main/java/com/example/retrofit/net/ApiConstant.java
@@ -5,10 +5,10 @@ public class ApiConstant {
     //生产
 //    public static final String JAVA_TEST_HOST = "http://oa.shanlinjinrong.com/oa-api/";
 
-//    public static final String JAVA_TEST_HOST = "http://testoa.shanlinjinrong.com/oa-api/";
+    public static final String JAVA_TEST_HOST = "http://testoa.shanlinjinrong.com/oa-api/";
 //    public static final String JAVA_TEST_HOST = "http://118.31.18.67:8090/";
     //新外网
 //    public static final String JAVA_TEST_HOST = "http://116.62.216.129:9105/";
     //新内网
-    public static final String JAVA_TEST_HOST = "http://10.5.202.122:9105/";
+//    public static final String JAVA_TEST_HOST = "http://10.5.202.122:9105/";
 }
