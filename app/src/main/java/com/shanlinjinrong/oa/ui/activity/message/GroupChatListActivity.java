0c48264cb5710b5e9bd5a5b9eac49a2d9b3578de
v_wangfengxu <Tonnywfx@gmail.com>
2017-12-14 12:40:35 +0800

    群组列表 搜索功能

--- app/src/main/java/com/shanlinjinrong/oa/ui/activity/message/GroupChatListActivity.java
+++ app/src/main/java/com/shanlinjinrong/oa/ui/activity/message/GroupChatListActivity.java
@@ -1,6 +1,7 @@
 package com.shanlinjinrong.oa.ui.activity.message;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -8,8 +9,10 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.TextView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
@@ -18,24 +21,28 @@ import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMGroup;
 import com.hyphenate.easeui.EaseConstant;
 import com.hyphenate.exceptions.HyphenateException;
-import com.iflytek.cloud.thirdparty.V;
+import com.jakewharton.rxbinding2.widget.RxTextView;
 import com.shanlinjinrong.oa.R;
 import com.shanlinjinrong.oa.manager.AppConfig;
 import com.shanlinjinrong.oa.manager.AppManager;
 import com.shanlinjinrong.oa.ui.activity.message.adapter.GroupChatListAdapter;
+import com.shanlinjinrong.oa.ui.base.BaseActivity;
+import com.shanlinjinrong.oa.views.ClearEditText;
 import com.shanlinjinrong.views.common.CommonTopView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 
 //聊天群组展示列表
-public class GroupChatListActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+public class GroupChatListActivity extends BaseActivity implements SwipeRefreshLayout.OnRefreshListener, View.OnKeyListener {
 
     @BindView(R.id.top_view)
     CommonTopView mTopView;
@@ -43,12 +50,16 @@ public class GroupChatListActivity extends AppCompatActivity implements SwipeRef
     RecyclerView mRvGroupShow;
     @BindView(R.id.sr_refresh)
     SwipeRefreshLayout mSrRefresh;
+    @BindView(R.id.search_et_input)
+    ClearEditText mSearchEtInput;
     @BindView(R.id.tv_error_layout)
     TextView tvErrorLayout;
 
     private View mFooterView;
     private GroupChatListAdapter mAdapter;
+    private InputMethodManager inputManager;
     private List<EMGroup> mGroupList = new ArrayList<>();
+    private List<EMGroup> mSearchGroupList = new ArrayList<>();
     @SuppressWarnings("SpellCheckingInspection")
     private final int REQUESTCODE = 101, DELETESUCCESS = -2, RESULTELECTEDCODE = -3;
 
@@ -70,8 +81,10 @@ public class GroupChatListActivity extends AppCompatActivity implements SwipeRef
             Intent intent = new Intent(this, SelectedGroupContactActivity.class);
             startActivityForResult(intent, REQUESTCODE);
         });
-
+        initSearchView();
+        mSearchEtInput.setOnKeyListener(this);
         mSrRefresh.setOnRefreshListener(this);
+        inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         mSrRefresh.setColorSchemeColors(Color.parseColor("#0EA7ED"), Color.parseColor("#0EA7ED"), Color.parseColor("#0EA7ED"));
         mSrRefresh.post(() -> mSrRefresh.setRefreshing(true));
         mAdapter = new GroupChatListAdapter(mGroupList);
@@ -80,6 +93,40 @@ public class GroupChatListActivity extends AppCompatActivity implements SwipeRef
         mRvGroupShow.addOnItemTouchListener(new ItemClick());
         mAdapter.notifyDataSetChanged();
         initFooterView();
+    }
+
+    private void initSearchView() {
+        //EditText 自动搜索,间隔->输入停止500毫秒后自动搜索
+        RxTextView.textChanges(mSearchEtInput)
+                .debounce(1000, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(charSequence -> {
+                    if (charSequence.toString().trim().equals("")) {
+                        showLoadingView();
+                        mAdapter.setNewData(mGroupList);
+                        mAdapter.notifyDataSetChanged();
+                        hideLoadingView();
+                    } else {
+                        showLoadingView();
+                        mSearchGroupList.clear();
+                        for (int i = 0; i < mGroupList.size(); i++) {
+                            if (mGroupList.get(i).getGroupName().contains(charSequence.toString().trim())) {
+                                mSearchGroupList.add(mGroupList.get(i));
+                            }
+                        }
+                        mAdapter.setNewData(mSearchGroupList);
+                        mAdapter.notifyDataSetChanged();
+                        hideLoadingView();
+                        //-------------------------------       键盘隐藏       -------------------------------
+                        try {
+                            inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                                    InputMethodManager.HIDE_NOT_ALWAYS);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
     }
 
     @SuppressLint("SetTextI18n")
@@ -164,6 +211,39 @@ public class GroupChatListActivity extends AppCompatActivity implements SwipeRef
     public void onRefresh() {
         initData();
         updateLayout();
+    }
+
+    @Override
+    public boolean onKey(View view, int j, KeyEvent keyEvent) {
+        if (j == KeyEvent.KEYCODE_ENTER && keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
+            //-------------------------------       键盘隐藏       -------------------------------
+            try {
+                inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                        InputMethodManager.HIDE_NOT_ALWAYS);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            if (mSearchEtInput.getText().toString().trim().equals("")) {
+                showLoadingView();
+                mAdapter.setNewData(mGroupList);
+                mAdapter.notifyDataSetChanged();
+                hideLoadingView();
+            } else {
+                showLoadingView();
+                mSearchGroupList.clear();
+                for (int i = 0; i < mGroupList.size(); i++) {
+                    if (mGroupList.get(i).getGroupName().contains(mSearchEtInput.getText().toString().trim())) {
+                        mSearchGroupList.add(mGroupList.get(i));
+                    }
+                }
+                mAdapter.setNewData(mSearchGroupList);
+                mAdapter.notifyDataSetChanged();
+                hideLoadingView();
+            }
+            return true;
+        }
+        return false;
     }
 
     public class ItemClick extends OnItemClickListener {
