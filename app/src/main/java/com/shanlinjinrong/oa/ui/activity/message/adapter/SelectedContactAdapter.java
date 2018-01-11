2b1bb3e0c8dfd8d659db9e5455ecb870688df8bc
v_wangfengxu <Tonnywfx@gmail.com>
2018-01-11 16:08:32 +0800

    update code

--- app/src/main/java/com/shanlinjinrong/oa/ui/activity/message/adapter/SelectedContactAdapter.java
+++ app/src/main/java/com/shanlinjinrong/oa/ui/activity/message/adapter/SelectedContactAdapter.java
@@ -60,7 +60,7 @@ public class SelectedContactAdapter extends BaseMultiItemQuickAdapter<Contacts> 
                 CircleImageView portrait = baseViewHolder.getView(R.id.portrait);
                 if (!TextUtils.isEmpty(contacts.getPortraits())) {
                     Glide.with(AppManager.mContext)
-                            .load(ApiConstant.BASE_PIC_URL + contacts.getPortraits())
+                            .load(contacts.getPortraits())
                             .dontAnimate()
                             .diskCacheStrategy(DiskCacheStrategy.ALL)
                             .placeholder(R.drawable.ease_user_portraits)
