f015eaf3e6a6923d43f9ad441f76d30b25b81704
huanghaibin <617305321@qq.com>
2018-01-17 13:39:50 +0800

    地址修改

--- retrofit/src/main/java/com/example/retrofit/net/ApiConstant.java
+++ retrofit/src/main/java/com/example/retrofit/net/ApiConstant.java
@@ -1,30 +1,48 @@
 package com.example.retrofit.net;
 
 public class ApiConstant {
+//TODO   图片地址
+    /**
+     * 生产环境
+     */
+//    public final static String BASE_PIC_URL = "http://public.oa.shanlinjinrong.com/";
+    /**
+     * 预发布环境  testoa
+     */
+//    public final static String BASE_PIC_URL = "http://public.testoa.shanlinjinrong.com";
+
+    /**
+     * 开发环境
+     */
+//    public final static String BASE_PIC_URL = "http://10.255.232.234:96";
+    /**
+     * 测试环境
+     */
+    public final static String BASE_PIC_URL = "http://10.5.202.122:9105";
+
+
+
+//TODO   接口地址
+    /**
+     * 生产环境
+     */
+    // public static final String JAVA_TEST_HOST = "http://oa.shanlinjinrong.com/oa-api/";
+
     /**
      * 新内网
      */
     // public static final String JAVA_TEST_HOST = "http://testoa.shanlinjinrong.com/oa-api/";
 
-    /**
-     * TODO 打包时候更改 善林图片前缀:public.sl.s1.zhitongoa.com
-     */
-//    public final static String BASE_PIC_URL = "http://public.testoa.shanlinjinrong.com";
-//    public final static String BASE_PIC_URL = "http://10.255.232.234:96";
-    //public final static String BASE_PIC_URL = "http://10.5.202.122:9105";
-
-    //生产环境图片地址
-    public final static String BASE_PIC_URL = "http://public.oa.shanlinjinrong.com/";
-
-    /**
-     * 生产环境
-     */
-      public static final String JAVA_TEST_HOST = "http://oa.shanlinjinrong.com/oa-api/";
 
     /**
      * 开发环境
      */
-    //public static final String JAVA_TEST_HOST = "http://10.255.232.234/oa-api/";
+    //public static final String JAVA_TEST_HOST = "http://10.255.232.234/oa-api/"; /**
+
+    /**
+     * 测试环境
+     */
+    public static final String JAVA_TEST_HOST = "http://10.5.202.122:9105/";
 
     /**
      * 新外网
