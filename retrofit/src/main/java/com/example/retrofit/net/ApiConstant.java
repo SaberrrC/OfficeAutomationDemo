2a0f067e2d034dfbee77aed0670be5b310fa0181
huanghaibin <617305321@qq.com>
2018-03-07 12:31:18 +0800

    我的申请  驳回添加删除

--- retrofit/src/main/java/com/example/retrofit/net/ApiConstant.java
+++ retrofit/src/main/java/com/example/retrofit/net/ApiConstant.java
@@ -5,21 +5,20 @@ public class ApiConstant {
     /**
      * 生产环境
      */
-//    public final static String BASE_PIC_URL = "http://public.oa.shanlinjinrong.com/";
+    //   public final static String BASE_PIC_URL = "http://public.oa.shanlinjinrong.com/";
     /**
      * 预发布环境  testoa
      */
-//        public final static String BASE_PIC_URL = "http://public.testoa.shanlinjinrong.com";
+    public final static String BASE_PIC_URL = "http://public.testoa.shanlinjinrong.com";
 
     /**
      * 开发环境
-     *
      */
-//        public final static String BASE_PIC_URL = "http://10.255.232.234:96";
+//    public final static String BASE_PIC_URL = "http://10.255.232.234:96";
     /**
      * 测试环境
      */
-        public final static String BASE_PIC_URL = "http://10.5.202.122:9105";
+//    public final static String BASE_PIC_URL = "http://10.5.202.122:9105";
 
 
     //TODO   接口地址
@@ -29,20 +28,20 @@ public class ApiConstant {
 //    public static final String JAVA_TEST_HOST = "http://oa.shanlinjinrong.com/oa-api/";
 
     /**
-     *  预发布环境  testoa
+     * 预发布环境  testoa
      */
-    //    public static final String JAVA_TEST_HOST = "http://testoa.shanlinjinrong.com/oa-api/";
+    public static final String JAVA_TEST_HOST = "http://testoa.shanlinjinrong.com/oa-api/";
 
 
     /**
      * 开发环境
      */
-//            public static final String JAVA_TEST_HOST = "http://10.255.232.234/oa-api/";
+//    public static final String JAVA_TEST_HOST = "http://10.255.232.234/oa-api/";
 
     /**
      * 测试环境
      */
-        public static final String JAVA_TEST_HOST = "http://10.5.202.122:9105/";
+//    public static final String JAVA_TEST_HOST = "http://10.5.202.122:9105/";
 
     /**
      * 新外网
@@ -50,3 +49,7 @@ public class ApiConstant {
     //    public static final String JAVA_TEST_HOST = "http://116.62.216.129:9105/";
 
 }
+
+//
+//密码：%$l73lh9u62!
+//密码：ilm5c5!wi^y#
