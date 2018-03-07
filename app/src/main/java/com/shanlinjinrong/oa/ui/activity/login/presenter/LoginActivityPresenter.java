1ccea2f77c503c9750a844f2f0a6f7b24669ef62
v_wangfengxu <Tonnywfx@gmail.com>
2018-03-07 14:38:32 +0800

    优化密码频繁错误 ，增加验证码

--- app/src/main/java/com/shanlinjinrong/oa/ui/activity/login/presenter/LoginActivityPresenter.java
+++ app/src/main/java/com/shanlinjinrong/oa/ui/activity/login/presenter/LoginActivityPresenter.java
@@ -5,6 +5,7 @@ import com.google.gson.reflect.TypeToken;
 import com.shanlinjinrong.oa.common.ApiJava;
 import com.shanlinjinrong.oa.model.UserInfo;
 import com.shanlinjinrong.oa.net.MyKjHttp;
+import com.shanlinjinrong.oa.ui.activity.login.bean.RequestCodeBean;
 import com.shanlinjinrong.oa.ui.activity.login.contract.LoginActivityContract;
 import com.shanlinjinrong.oa.ui.base.HttpPresenter;
 import com.shanlinjinrong.oa.utils.LogUtils;
@@ -42,8 +43,9 @@ public class LoginActivityPresenter extends HttpPresenter<LoginActivityContract.
 
                     switch (user.getCode()) {
                         case ApiJava.REQUEST_CODE_OK:
-                            if (mView != null)
+                            if (mView != null) {
                                 mView.loginSuccess(user.getData());
+                            }
                             break;
                         case ApiJava.REQUEST_TOKEN_NOT_EXIST:
                         case ApiJava.REQUEST_TOKEN_OUT_TIME:
@@ -59,6 +61,27 @@ public class LoginActivityPresenter extends HttpPresenter<LoginActivityContract.
                             }
                             break;
                         default:
+                            if (mView != null) {
+                                if ("100169".equals(user.getCode())) {
+                                    mView.showVerifyCode(user.getMessage());
+                                } else if ("100016".equals(user.getCode())) {
+                                    try {
+                                        if (user.getData() != null) {
+                                            if (user.getData().getNeedYzm() != null) {
+                                                if ("1".equals(user.getData().getNeedYzm())) {
+                                                    mView.showVerifyCode(user.getMessage());
+                                                }
+                                            }
+                                        }else {
+                                            mView.accountOrPswError(user.getCode(), user.getMessage());
+                                        }
+                                    } catch (Throwable e) {
+                                        e.printStackTrace();
+                                    }
+                                } else {
+                                    mView.accountOrPswError(user.getCode(), user.getMessage());
+                                }
+                            }
                             break;
                     }
                 } catch (Throwable e) {
@@ -86,6 +109,142 @@ public class LoginActivityPresenter extends HttpPresenter<LoginActivityContract.
                 try {
                     if (mView != null) {
                         mView.requestFinish();
+                    }
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void login(String account, String psw, String keyCode, String code) {
+        mKjHttp.cleanCache();
+        HttpParams params = new HttpParams();
+        params.put("email", account);
+        params.put("pwd", psw);
+        params.put("keyCode", keyCode);
+        params.put("code", code);
+        mKjHttp.post(ApiJava.LOGIN, params, new HttpCallBack() {
+            @Override
+            public void onSuccess(String t) {
+                super.onSuccess(t);
+                System.out.println(t);
+                try {
+                    UserInfo user = new Gson().fromJson(t, new TypeToken<UserInfo>() {
+                    }.getType());
+
+                    switch (user.getCode()) {
+                        case ApiJava.REQUEST_CODE_OK:
+                            if (mView != null) {
+                                mView.loginSuccess(user.getData());
+                            }
+                            break;
+                        case ApiJava.REQUEST_TOKEN_NOT_EXIST:
+                        case ApiJava.REQUEST_TOKEN_OUT_TIME:
+                        case ApiJava.ERROR_TOKEN:
+                            if (mView != null) {
+                                mView.loginFailed(user.getCode());
+                            }
+                            break;
+                        //用户名 密码不存在
+                        case ApiJava.NOT_EXIST_USER:
+                            if (mView != null) {
+                                mView.accountOrPswError(user.getMessage());
+                            }
+                            break;
+                        case "300001":
+                            if (mView!=null){
+                                mView.refreshVerifyCode(user.getMessage());
+                            }
+                            break;
+                        default:
+                            if (mView != null) {
+                                if ("100169".equals(user.getCode())) {
+                                    mView.showVerifyCode(user.getMessage());
+                                } else if ("100016".equals(user.getCode())) {
+                                    try {
+                                        if (user.getData() != null) {
+                                            if (user.getData().getNeedYzm() != null) {
+                                                if ("1".equals(user.getData().getNeedYzm())) {
+                                                    mView.showVerifyCode(user.getMessage());
+                                                }
+                                            }
+                                        }
+                                    } catch (Throwable e) {
+                                        e.printStackTrace();
+                                    }
+                                } else {
+                                    mView.accountOrPswError(user.getCode(), user.getMessage());
+                                }
+                            }
+                            break;
+                    }
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int errorNo, String strMsg) {
+                LogUtils.e(errorNo + "--" + strMsg);
+                try {
+                    if (mView != null) {
+                        mView.uidNull(strMsg);
+                        mView.loginFailed(String.valueOf(errorNo));
+                    }
+                    super.onFailure(errorNo, strMsg);
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFinish() {
+                super.onFinish();
+                try {
+                    if (mView != null) {
+                        mView.requestFinish();
+                    }
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void QueryVerifyCode() {
+        mKjHttp.cleanCache();
+        mKjHttp.get(ApiJava.SENDS_CAPTCHA, new HttpParams(), new HttpCallBack() {
+            @Override
+            public void onSuccess(String t) {
+                super.onSuccess(t);
+                try {
+                    RequestCodeBean requestStatus = new Gson().fromJson(t, new TypeToken<RequestCodeBean>() {
+                    }.getType());
+
+                    switch (requestStatus.getCode()) {
+                        case ApiJava.REQUEST_CODE_OK:
+                            if (mView != null) {
+                                mView.getIdentifyingCodeSuccess(requestStatus.getData().getImg(), requestStatus.getData().getKeyCode());
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                } catch (Throwable throwable) {
+                    throwable.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int errorNo, String strMsg) {
+                super.onFailure(errorNo, strMsg);
+                try {
+                    if (mView != null) {
+                        mView.uidNull(strMsg);
+                        mView.getIdentifyingCodeFailed(errorNo);
                     }
                 } catch (Throwable e) {
                     e.printStackTrace();
