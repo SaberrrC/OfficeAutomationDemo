3f5993607820c9dc5aa811e2b0aff671e23308cc
huanghaibin <617305321@qq.com>
2018-03-21 21:52:20 +0800

    url  update

--- retrofit/src/main/java/com/example/retrofit/net/ApiConstant.java
+++ retrofit/src/main/java/com/example/retrofit/net/ApiConstant.java
@@ -5,11 +5,11 @@ public class ApiConstant {
     /**
      * 生产环境
      */
-//       public final static String BASE_PIC_URL = "https://public-oa.shanlinjinrong.com/";
+       public final static String BASE_PIC_URL = "https://public-oa.shanlinjinrong.com/";
     /**
      * 预发布环境  testoa
      */
-    public final static String BASE_PIC_URL = "http://public.testoa.shanlinjinrong.com";
+//    public final static String BASE_PIC_URL = "http://public.testoa.shanlinjinrong.com";
 
     /**
      * 开发环境
@@ -26,12 +26,12 @@ public class ApiConstant {
     /**
      * 生产环境
      */
-    //public static final String JAVA_TEST_HOST = "https://oa.shanlinjinrong.com/oa-api/";  public static final String baseUrl= "0";
+    public static final String JAVA_TEST_HOST = "https://oa.shanlinjinrong.com/oa-api/";  public static final String baseUrl= "0";
 
     /**
      * 预发布环境  testoa
      */
-    public static final String JAVA_TEST_HOST = "http://testoa.shanlinjinrong.com/oa-api/";  public static final String baseUrl= "1";
+//    public static final String JAVA_TEST_HOST = "http://testoa.shanlinjinrong.com/oa-api/";  public static final String baseUrl= "1";
 
 
     /**
